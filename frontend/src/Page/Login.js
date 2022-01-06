ca9bbaccfb4058fcc6e9ca4f7796e964e24a141e
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-06 22:34:50 +0530

    ui

--- frontend/src/Page/Login.js
+++ frontend/src/Page/Login.js
@@ -1,11 +1,64 @@
-import React from 'react'
-
-function Login() {
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  
+  export default function Login() {
     return (
-        <div>
-            h1Login
-        </div>
-    )
-}
+      <Flex
+        minH={'auto'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+            
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+               
+                <Button
+                  bg={'green.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'green.600',
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
 
-export default Login
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Don't have a account? <Link color={'blue.400'}>Register</Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
