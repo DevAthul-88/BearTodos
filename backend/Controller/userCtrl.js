c64a817a02faf141eb3788719f09cafcaca0537b
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-07 12:07:24 +0530

    user auth

--- backend/Controller/userCtrl.js
+++ backend/Controller/userCtrl.js
@@ -1,19 +1,73 @@
+require('dotenv').config()
+const User = require("../Models/userSchema");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
 module.exports = {
+  getUser: async (req , res) => {
+      try {
 
-    getUser: () => {
+          const {email , password } = req.body
 
-    },
+          const user = await User.findOne({email: email})
+
+          if(!user) return res.status(404).send({message: "User not found"})
+
+          const validPassword = await bcrypt.compare(user.password , password)
+
+          if(!validPassword) return res.status(400).send({message: "Invalid password"})
+
+          const payload = {
+           
+              id:user._id,
+
+          }
+
+          const token = jwt.sign(payload , process.env.Token , {
+              expiresIn:"30d"
+          })
+
+          payload.token = token
+          res.send({token: token})
+
+          
+      } catch (error) {
+          res.status(500).send({message: error.message});
+      }
+  },
+
+  createUser: async (req, res) => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+
+      const existingUser = await User.findOne({email:email})
+
+      if(existingUser) return res.send({message: "User already exists"})
+
+      const hashedPassword = await bcrypt.hash(password , 10)
+
+      const user = new User({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: hashedPassword,
+      })
+
+      user.save()
+      res.status(200).send({loggedIn: true})
+
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+  },
+
+  verifyUser: (req , res) => {
+
     
 
-    createUser: () => {
+  },
 
-    },
+  editUser: () => {},
 
-    editUser: () => {
-
-    },
-
-    deleteUser: () => {
-
-    }
-}
+  deleteUser: () => {},
+};
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
+mongoose.connect(process.env.MONGO_URL , (err) => {
+    if(err) return console.error(err)
+    console.log('Database connection established');
+})
+
 app.use('/user' , userRoute)
+
+
+
 
 app.listen(process.env.PORT,  () => {
     console.log(`Server started on port ${process.env.PORT}`);
